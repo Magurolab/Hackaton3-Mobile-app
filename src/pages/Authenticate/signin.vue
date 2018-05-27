93d878228a26a646031dda74aad75b1d0809f48e
Siripatsorn <pinky.siripatsorn@gmail.com>
2018-05-27 14:22:57 +0700

    add signin page

---
+++ src/pages/Authenticate/signin.vue
@@ -0,0 +1,33 @@
+<template>
+  <f7-page login-screen>
+    <f7-login-screen-title>Login</f7-login-screen-title>
+    <f7-list form>
+      <f7-list-item>
+        <f7-label>Username</f7-label>
+        <f7-input name="username" placeholder="Username" type="text"></f7-input>
+      </f7-list-item>
+      <f7-list-item>
+        <f7-label>Password</f7-label>
+        <f7-input name="password" type="password" placeholder="Password"></f7-input>
+      </f7-list-item>
+    </f7-list>
+
+    <f7-list>
+      <f7-list-button title="Sign In" login-screen-close></f7-list-button>
+      <f7-block-footer>
+        <p>Don't have an account ? </p>
+      </f7-block-footer>
+      <f7-link href="/signup/">Signup for Aibay</f7-link>
+    </f7-list>
+  </f7-page>
+</template>
+
+<script>
+
+  import F7View from "framework7-vue/src/components/view";
+  import F7Page from "framework7-vue/src/components/page";
+
+  export default {components: {
+      F7Page,
+      F7View}}
+</script>
--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,25 @@ import HomePage from './pages/home.vue';
 import AboutPage from './pages/about.vue';
 import FormPage from './pages/form.vue';
 import DynamicRoutePage from './pages/dynamic-route.vue';
-import NotFoundPage from './pages/not-found.vue';
+import NotFoundPage from './pages/Authenticate/not-found.vue';
 
 import PanelLeftPage from './pages/panel-left.vue';
 import PanelRightPage from './pages/panel-right.vue';
 
+import Signin from './pages/Authenticate/signin'
+
 export default [
+  // {
+  //   path: '/',
+  //   component: HomePage,
+  // },
   {
     path: '/',
-    component: HomePage,
+    component: Signin,
+  },
+  {
+    path: '/',
+    component: Signin,
   },
   {
     path: '/panel-left/',
