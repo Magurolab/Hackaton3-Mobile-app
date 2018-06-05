080f31c4686f8154c745e79f610e4a633cf36d9a
Siripatsorn <pinky.siripatsorn@gmail.com>
2018-06-05 15:11:08 +0700

    fixprofile page

--- src/pages/User/Profile.vue
+++ src/pages/User/Profile.vue
@@ -4,16 +4,12 @@
 
     <div class="card demo-facebook-card">
       <div class="card-header">
-        <div class="demo-facebook-name">{{userInfo().username}}</div>
-        <div class="demo-facebook-date">Email: {{userEmail()}}</div>
-        <div class="demo-facebook-date">University: {{userInfo().university}}</div>
-        <div class="demo-facebook-date">Description: {{userInfo().description}}</div>
+        <div class="demo-facebook-name">{{userInfo.username}}</div>
+        <div class="demo-facebook-date">Email: {{userInfo.email}}</div>
+        <div class="demo-facebook-date">University: {{userInfo.university}}</div>
+        <div class="demo-facebook-date">Description: {{userInfo.description}}</div>
       </div>
-      <div class="card-content card-content-padding">
-        <p>University</p>
-        <p>Description</p>
-      </div>
-      <f7-button @click="Whoami" >Who am I</f7-button>
+      <!--<f7-button @click="Whoami" >Who am I</f7-button>-->
       <f7-button @click="signOut" >Log out</f7-button>
     </div>
 
@@ -35,16 +31,18 @@
       F7View,
       auth, db
     },
-    methods: {
-      userInfo(){
-        var userInfo = null;
+    computed: {
+      userInfo() {
+        var currentuserInfo = null;
         const uid = auth.currentUser.uid
         const ref = db.ref('Users/' + uid)
         ref.on('value', function (snapshot) {
-          userInfo = snapshot.val()
+          currentuserInfo = snapshot.val()
         })
-        return userInfo
+        return currentuserInfo
       },
+    },
+    methods: {
       userEmail(){
         return auth.currentUser.email
       },
@@ -54,9 +52,9 @@
         })
         this.$f7router.navigate('/signin/')
       },
-      Whoami() {
-        console.log("current user ",auth.currentUser.email)
-      }
+      // Whoami() {
+      //   console.log("current user ",auth.currentUser.email)
+      // }
     },
     created: function () {
       console.log('current user on profile', auth.currentUser)
